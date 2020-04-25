7eb5f564064478bc09403edbd3820d5d671df90d
pablo Coronel <pablomanuelcoronel@gmail.com>
2020-04-25 18:19:03 -0300

    scripts en package.json  and run express

---
+++ src/server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+
+// Initials
+const app = express();
+
+// Settings
+
+// Middlewares
+
+// Global variables
+
+// Routes
+
+// Static files
+
+module.exports = app;
