0abcc738c96d60ed901c847380dba6440ab73793
pablo Coronel <pablomanuelcoronel@gmail.com>
2020-05-03 19:09:06 -0300

    fix in path of navigation, added routes for editions

--- src/controllers/ediciones.controller.js
+++ src/controllers/ediciones.controller.js
@@ -1,7 +1,17 @@
 const edicionesController = {};
 
+// WEB
 edicionesController.renderEdiciones = (req, res) => {
 	res.render('ediciones');
+};
+
+// CRUD
+edicionesController.renderEdicionesCrear = (req, res) => {
+	res.render('models/ediciones/create');
+};
+
+edicionesController.createEdiciones = (req, res) => {
+	console.log('guardar edicion');
 };
 
 module.exports = edicionesController;
--- src/routes/ediciones.routes.js
+++ src/routes/ediciones.routes.js
@@ -2,9 +2,15 @@ const { Router } = require('express');
 const router = Router();
 
 // Funciones
-const { renderEdiciones } = require('../controllers/ediciones.controller');
+const {
+	renderEdiciones,
+    renderEdicionesCrear,
+    createEdiciones
+} = require('../controllers/ediciones.controller');
 
 // Rutas
 router.get('/ediciones', renderEdiciones);
+router.get('/ediciones/crear', renderEdicionesCrear);
+router.post('/ediciones/crear', createEdiciones)
 
 module.exports = router;
