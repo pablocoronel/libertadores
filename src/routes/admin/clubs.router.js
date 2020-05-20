c2f173354dff4e37bb07760e4d72e964f2f5134b
pablo Coronel <pablomanuelcoronel@gmail.com>
2020-05-19 21:24:01 -0300

    rename validation file

--- src/routes/admin/clubs.router.js
+++ src/routes/admin/clubs.router.js
@@ -3,7 +3,7 @@ const router = Router();
 const path = require('path');
 const multer = require('multer'); // sirve para subir archivos desde form
 const upload = multer({ dest: path.join(__dirname, '../../public/images/clubs') });
-const validator = require('../../middlewares/validator');
+const validation = require('../../middlewares/validation');
 
 // Funciones
 const {
@@ -20,13 +20,13 @@ router.get('/admin/clubs', listClubs);
 router.get('/admin/clubs/create', createClubs);
 router.post(
 	'/admin/clubs',
-	[upload.single('shield'), validator.storeClub],
+	[upload.single('shield'), validation.storeClub],
 	storeClubs
 );
 router.get('/admin/clubs/:id/edit', editClubs);
 router.put(
 	'/admin/clubs/:id',
-	[upload.single('shield'), validator.updateClub],
+	[upload.single('shield'), validation.updateClub],
 	updateClubs
 );
 router.delete('/admin/clubs/:id', destroyClubs);
--- src/routes/admin/editions.routes.js
+++ src/routes/admin/editions.routes.js
@@ -5,7 +5,7 @@ const multer = require('multer');
 const upload = multer({
 	dest: path.join(__dirname, '../../public/images/covers'),
 });
-const validator = require('../../middlewares/validator');
+const validation = require('../../middlewares/validation');
 
 // Funciones
 const {
@@ -24,7 +24,7 @@ router.post(
 	'/admin/editions',
 	[
 		upload.fields([{ name: 'cover' }, { name: 'squad' }]),
-		validator.storeEdition,
+		validation.storeEdition,
 	],
 	storeEditions
 );
@@ -33,7 +33,7 @@ router.put(
 	'/admin/editions/:id',
 	[
 		upload.fields([{ name: 'cover' }, { name: 'squad' }]),
-		validator.updateEdition,
+		validation.updateEdition,
 	],
 	updateEditions
 );
