419ff8a9dd57921bbdc1b28d6ad32802b8ecd421
pablo Coronel <pablomanuelcoronel@gmail.com>
2020-05-21 22:50:27 -0300

    done render for list of editions

--- src/controllers/editions.controller.js
+++ src/controllers/editions.controller.js
@@ -1,8 +1,22 @@
 const editionsController = {};
+const Edition = require('../models/Edition');
 
 // Funciones
-editionsController.renderEditions = (req, res) => {
-	res.render('editions');
+editionsController.renderEditions = async (req, res) => {
+	try {
+		const allEditions = await Edition.find().sort({ year: 'asc' });
+		const matrixEditions = [];
+
+		allEditions.map((edition, index) => {
+			if (index % 5 == 0) {
+				matrixEditions.push(allEditions.slice(index, index + 5));
+			}
+		});
+
+		res.render('editions', { editions: matrixEditions });
+	} catch (error) {
+		console.log(error);
+	}
 };
 
 module.exports = editionsController;
