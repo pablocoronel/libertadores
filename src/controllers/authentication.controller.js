89dec23fd381cb50413fa0bf6c23dee558af6beb
pablo Coronel <pablomanuelcoronel@gmail.com>
2020-07-10 21:47:52 -0300

    done local strategy for auth with passpost

--- src/controllers/authentication.controller.js
+++ src/controllers/authentication.controller.js
@@ -1,10 +1,20 @@
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const User = require('../models/User');
+const bcrypt = require('bcrypt');
+
 const authenticationController = {};
 
 authenticationController.renderSignup = (req, res) => {
 	res.render('signup');
 };
 
-authenticationController.signup = (req, res) => {};
+authenticationController.signup = passport.authenticate('local-signup', {
+	successRedirect: '/login',
+	failureRedirect: '/signup',
+	failureFlash: { type: 'messageError', message: 'Mal' },
+	successFlash: { type: 'successMessage', message: 'Ok' },
+});
 
 authenticationController.renderLogin = (req, res) => {
 	res.render('login');
@@ -13,5 +23,41 @@ authenticationController.renderLogin = (req, res) => {
 authenticationController.login = (req, res) => {};
 
 authenticationController.logout = (req, res) => {};
+
+passport.use(
+	'local-signup',
+	new LocalStrategy(
+		{
+			usernameField: 'email',
+			passwordField: 'password',
+			passReqToCallback: true,
+		},
+		async (req, email, password, done) => {
+			let user = await User.find({ email });
+			// si ya existe el usurio...
+			if (user) {
+				return done(
+					null,
+					false,
+					req.flash(
+						'messageError',
+						'El email ya se encuentra registrado'
+					)
+				);
+			}
+
+			// registrar nuevo usuario
+			const newUser = new User({
+				email,
+				user: req.body.user,
+				password: await bcrypt.hash(password),
+			});
+
+			await newUser.save();
+
+			return done(null, newUser);
+		}
+	)
+);
 
 module.exports = authenticationController;
