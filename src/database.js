4ed3ebf5045071deb5dafe0cb8dd7a0903b36f88
pablo Coronel <pablomanuelcoronel@gmail.com>
2020-08-09 23:24:30 -0300

    example env file added

--- src/database.js
+++ src/database.js
@@ -1,10 +1,10 @@
 const mongoose = require('mongoose');
 
 // Datos de conexion
-const { MONGODB_DATABASE, MONGOBD_HOST } = process.env;
+const { MONGODB_PROTOCOL, MONGODB_DATABASE, MONGODB_HOST } = process.env;
 
 // Url conexion
-const MONGODB_URI = `mongodb://${MONGOBD_HOST}/${MONGODB_DATABASE}`;
+const MONGODB_URI = `${MONGODB_PROTOCOL}://${MONGODB_HOST}/${MONGODB_DATABASE}`;
 
 // Conexion
 mongoose
