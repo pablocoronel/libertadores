508bc903bc57161abfc8111b0ad6fdb14333daed
pablo Coronel <pablomanuelcoronel@gmail.com>
2020-08-16 18:59:15 -0300

    node_env variable setted for dev and prod

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
-require('./config/env.config'); // Carga las variables en .env desde un archivo de configuracion
-
+// require('./config/env.config'); // Carga las variables en .env desde un archivo de configuracion
+if (process.env.NODE_ENV === 'development') {
+	require('dotenv').config(); // carga el .env
+}
 const app = require('./server'); // importa el server.js
 
 require('./database'); // importa la configuracion de la BD
